f9fa4627d45cfa455ec0473679af61b856268e2f
itsnestee <nesteemusicprovvidence@gmail.com>
2022-08-05 19:01:39 +0200

    Building Public

--- SimpleServer.js
+++ SimpleServer.js
@@ -1,2 +1,24 @@
-var http = require("http");
+//Dependencies
+var http = require("http"); /* Load HTTP Library*/
+var express = require("express"); /* Load Express Dependency */
+
+//App
+var app = express(); /* Create App */
+
+//Variable
+var port = 8888; /* port */
+
+app.set('view engine' , 'ejs');
+app.use(express.static('public'))
+
+/*Basic Routing server*/ 
+app.get('/',(req, res) => {
+    console.log("HTML OUT");
+
+   // res.render("index"); //Response to browser
+});
+
+
+app.listen(port);
+
 
