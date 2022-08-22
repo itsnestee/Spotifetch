4f359d2b2c5163fef7e0c96c5cf6cd4ad81828ea
itsnestee <nesteemusicprovvidence@gmail.com>
2022-08-22 11:41:35 +0200

    Server created

--- SimpleServer.js
+++ SimpleServer.js
@@ -1,12 +1,30 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+var port = 8888; //port 
+
+
+
+
+
+
+
+var SpotifyWebApi = require('spotify-web-api-node');
+
+
+
+
 var scopes = ['user-read-private', 'user-read-email'],
-  redirectUri = 'https://example.com/callback',
-  clientId = '5fe01282e44241328a84e7c5cc169165',
-  state = 'some-state-of-my-choice';
+    redirectUri = 'http://localhost:8888/callback/',
+    clientId = '24ff0e4c598c4437947c77f5e3c80f70',
+    state = 'some-state-of-my-choice';
 
 // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
 var spotifyApi = new SpotifyWebApi({
-  redirectUri: redirectUri,
-  clientId: clientId
+    redirectUri: redirectUri,
+    clientId: clientId
 });
 
 // Create the authorization URL
@@ -14,3 +32,7 @@ var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
 
 // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
 console.log(authorizeURL);
+
+
+console.log("Port Listening...");
+app.listen(port);
