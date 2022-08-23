420430ec6dbfa56c941c14e13016321b00b0edf6
itsnestee <nesteemusicprovvidence@gmail.com>
2022-08-23 16:20:36 +0200

    Token Achieved

--- SimpleServer.js
+++ SimpleServer.js
@@ -1,38 +1,90 @@
-var express = require('express');
+var express = require('express'); //web app instantiation
+var SpotifyWebApi = require('spotify-web-api-node');//library instantiation
+var app = express(); // calling app
 
-var app = express();
 
-app.use(express.static('public'));
+app.use(express.static('public')); //rendering files to browser
+
+
 var port = 8888; //port 
-
-
-
-
-
-
-
-var SpotifyWebApi = require('spotify-web-api-node');
-
-
 
 
 var scopes = ['user-read-private', 'user-read-email'],
     redirectUri = 'http://localhost:8888/callback/',
     clientId = '24ff0e4c598c4437947c77f5e3c80f70',
+    clientSec = '',
     state = 'some-state-of-my-choice';
 
-// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
-var spotifyApi = new SpotifyWebApi({
+
+var spotifyApi = new SpotifyWebApi({ // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
     redirectUri: redirectUri,
-    clientId: clientId
+    clientId: clientId,
+    clientSecret: clientSec
 });
 
-// Create the authorization URL
-var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);// Create the authorization URL
 
+app.get('/login', (req, res) => { //Login request to user
+    res.redirect(authorizeURL);
+})
+
+app.get('/callback', (req, res) => {//if error give error if not get token
+    var error = req.query.error;
+    var code = req.query.code;
+    var state = req.query.state;
+
+
+    if (error) {
+        console.error('Yoo Error:', error);
+        res.send('Callback error: ${error}');
+        return;
+    }
+
+    spotifyApi
+        .authorizationCodeGrant(code)
+        .then(data => {
+            const access_token = data.body['access_token'];
+            const refresh_token = data.body['refresh_token'];
+            const expires_in = data.body['expires_in'];
+
+
+
+            spotifyApi.setAccessToken(access_token);
+            spotifyApi.setRefreshToken(refresh_token);
+
+            console.log('access_token:', access_token);
+            console.log('refresh_token:', refresh_token);
+
+            console.log(
+                `Sucessfully retreived access token. Expires in ${expires_in} s.`
+            );
+
+
+            res.sendFile('OK SERVER');
+
+
+
+            setInterval(async () => {
+                const data = await spotifyApi.refreshAccessToken();
+                const access_token = data.body['access_token'];
+
+                console.log('The access token has been refreshed!');
+                console.log('access_token:', access_token);
+                spotifyApi.setAccessToken(access_token);
+            }, expires_in / 2 * 1000);
+        })
+        .catch(error => {
+            console.error('Error getting Tokens:', error);
+            res.send(`Error getting Tokens: ${error}`);
+        });
+
+});
 // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
 console.log(authorizeURL);
 
 
 console.log("Port Listening...");
 app.listen(port);
+
+
+
